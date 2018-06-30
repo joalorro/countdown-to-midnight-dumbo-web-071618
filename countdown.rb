6efa21e01a9d7d717e73f9a39ef4b77f1cfeedae
Josue V. Alorro <joalorro@gmail.com>
2018-06-30 13:37:18 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,12 +4,16 @@ def countdown (secs)
   while secs > 0
     puts "#{secs} SECOND(S)!"
     secs -= 1 
-    sleep(1)
   end
   
   return "HAPPY NEW YEAR!"
 end
 
 def count_down_with_sleep(1)
-  sleep(1)
+  while secs > 0
+    puts "#{secs} SECOND(S)!"
+    secs -= 1 
+  end
+  
+  return "HAPPY NEW YEAR!"
 end
